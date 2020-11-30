74e4776c9a496b7eb89c27ec8d8373dc64a23e14
Decave <decave27@gmail.com>
2020-11-30 13:04:39 +0900

    Update setup.py
    readme update

--- setup.py
+++ setup.py
@@ -11,7 +11,9 @@ with open("autostock/__init__.py", encoding="UTF8") as f:
 path = = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
 with open(f"{path}/requirements.txt", encoding="UTF8") as f:
     requirements = f.read().splitlines()
-    
+
+with open(f"README.md", encoding="UTF8") as f:
+    readme = f.read()
 setup(
     name = 'autostock',
     version = version,
@@ -19,7 +21,7 @@ setup(
     author = 'decave27',
     author_email = 'decave27@gmail.com',
     description = 'Making stock functions easy',
-    long_description = '[github](https://github.com/decave27/autostock)',
+    long_description = readme,
     long_description_content_type = 'text/markdown',
     url = 'hhttps://github.com/decave27/autostock',
     packages = find_packages(),
